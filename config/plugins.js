984a4d51f0d59dca3d17fc79a91b1393ce525d64
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2022-08-16 12:56:43 -0300

    ter 16 ago 2022 12:56:43 -03 => plugin documentation instalado

---
+++ config/plugins.js
@@ -0,0 +1,18 @@
+module.exports = ({env}) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-digitalocean-nohash", 
+      providerOptions: {
+        includeHash: true, // rename files with MD5 hash
+        key: env('DO_SPACE_ACCESS_KEY'),
+        secret: env('DO_SPACE_SECRET_KEY'),
+        endpoint: env('DO_SPACE_ENDPOINT'),
+        space: env('DO_SPACE_BUCKET'),
+        directory: env('DO_SPACE_DIRECTORY'),
+        cdn: env('DO_SPACE_CDN'),
+      }
+    },
+  }, 
+  // ...
+})

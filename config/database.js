111e0929cf9e7a288299ed26b77067c5787eee17
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2022-08-16 11:02:38 -0300

    ter 16 ago 2022 11:02:38 -03 => plugin documentation instalado

--- config/database.js
+++ config/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         "app-91510196-b781-4b38-83f4-9700f3be635e-do-user-11643467-0.b.db.ondigitalocean.com"
       ),
       port: env.int("DATABASE_PORT", 25060),
-      database: env("DATABASE_NAME", "db"),
-      user: env("DATABASE_USERNAME", "db"),
-      password: env("DATABASE_PASSWORD", "AVNS_aCr0Sx9OVkslqage0TX"),
+      database: env("DATABASE_NAME", "defaultdb"),
+      user: env("DATABASE_USERNAME", "doadmin"),
+      password: env("DATABASE_PASSWORD", "AVNS_kPjGtHBB39jSlMfa5Wp"),
       ssl: {
         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
       },

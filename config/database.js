e66244150e128ce9165b38798226e272d68cf1c5
gusleaooliveira <gus.leaono@gmail.com>
2022-08-15 23:17:31 -0300

    seg 15 ago 2022 23:17:31 -03

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env(
+        "DATABASE_HOST",
+        "app-91510196-b781-4b38-83f4-9700f3be635e-do-user-11643467-0.b.db.ondigitalocean.com"
+      ),
+      port: env.int("DATABASE_PORT", 25060),
+      database: env("DATABASE_NAME", "db"),
+      user: env("DATABASE_USERNAME", "db"),
+      password: env("DATABASE_PASSWORD", "AVNS_aCr0Sx9OVkslqage0TX"),
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     useNullAsDefault: true,
+    debug: true,
   },
 });

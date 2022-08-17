3540dac8c51c729523d89d244be0c34acd72c214
Gustavo Leão <gustavo.leao.nogueira.2017@gmail.com>
2022-08-17 11:24:10 -0300

    qua 17 ago 2022 11:24:10 -03 => common questions]

--- src/api/commom-question/controllers/commom-question.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  commom-question controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::commom-question.commom-question');
--- src/api/commom-question/routes/commom-question.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * commom-question router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::commom-question.commom-question');
